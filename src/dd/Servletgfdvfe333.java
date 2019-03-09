5b5ae82c0a1b197cf85628e88138290e482844a2
862532665 <862532665@qq.com>
2019-03-10 04:42:13 +0800

    mm

---
+++ src/dd/Servletgfdvfe333.java
@@ -0,0 +1,22 @@
+package dd;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author: 11827
+ * @date: 2019/3/9 01:53
+ * @description:
+ */
+public class Servletgfdvfe333 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
